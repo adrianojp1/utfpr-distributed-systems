846e7baccfff5431cc476381a25999ef4de04d86
Adriano Paulichi <adriano.paulichi@localiza.com>
2023-03-27 21:35:15 -0300

    Work 1: receive and send using argv's

--- work1/src/receive.py
+++ work1/src/receive.py
@@ -1,22 +1,40 @@
 #!/usr/bin/env python3
+import os
+import sys
 import pika
 
 def callback(ch, method, properties, body):
     print("Received %s" % body)
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
+def main():
+    if len(sys.argv) == 1:
+        print('A exchange name must be provided!')
+        print('Usage: receive.py {exchange-name}')
+        sys.exit(1)
 
-channel.exchange_declare(exchange='notifications', exchange_type='fanout')
+    exchange_name = sys.argv[1]
+    
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
 
-result = channel.queue_declare(queue='', exclusive=True)
-queue_name = result.method.queue
+    channel.exchange_declare(exchange=exchange_name, exchange_type='fanout')
+    result = channel.queue_declare(queue='', exclusive=True)
 
-channel.queue_bind(exchange='notifications', queue=queue_name)
+    queue_name = result.method.queue
+    channel.queue_bind(exchange=exchange_name, queue=queue_name)
 
-print('Waiting for logs...')
+    print(f'Listening messages on exchange {exchange_name}. To exit press CTRL+C')
 
-channel.basic_consume(
-    queue=queue_name, on_message_callback=callback, auto_ack=True)
+    channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+    channel.start_consuming()
 
-channel.start_consuming()
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('Interrupted')
+        try:
+            sys.exit(0)
+        except SystemExit:
+            os._exit(0)
--- work1/src/send.py
+++ work1/src/send.py
@@ -2,18 +2,33 @@
 import sys
 import pika
 
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
+def main():
+    usage_msg = 'Usage: receive.py {exchange-name} {message-content}'
+    if len(sys.argv) == 1:
+        print('A exchange name and message content must be provided!')
+        print(usage_msg)
+        sys.exit(1)
+    elif len(sys.argv) == 2:
+        print('A message content must be provided!')
+        print(usage_msg)
+        sys.exit(2)
 
-channel.exchange_declare(exchange='notifications',
-                         exchange_type='fanout')
+    exchange_name = sys.argv[1]
+    message = ' '.join(sys.argv[2:])
 
-message = ' '.join(sys.argv[1:]) or "default message: Hello World!"
+    connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+    channel = connection.channel()
 
-channel.basic_publish(
-    exchange='notifications',
-    routing_key='',
-    body=message
-)
-print("Sent '%s'" % message)
-connection.close()
+    channel.exchange_declare(exchange=exchange_name, exchange_type='fanout')
+
+    channel.basic_publish(
+        exchange=exchange_name,
+        routing_key='',
+        body=message
+    )
+    print(f"Message sent to exchange. exchange: {exchange_name}, message '{message}'")
+    connection.close()
+
+
+if __name__ == '__main__':
+    main()
